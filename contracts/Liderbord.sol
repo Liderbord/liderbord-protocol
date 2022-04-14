dac4fd5d6f4f6bffb0ac1132e47c82aa015ed3a3
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2022-04-14 14:56:04 +0200

    feat: vote & delete resource

--- contracts/Liderbord.sol
+++ contracts/Liderbord.sol
@@ -3,15 +3,23 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract Liderbord {
+contract Liderbords {
+    struct Score {
+        uint value;
+        uint index;
+    }
     struct Resource {
-        mapping(string => uint) scores;
+        mapping(string => Score) scores;
         string[] liderbords;
-        mapping(address => uint) voters;
         address owner;
     }
+    struct Liderbord {
+        uint length;
+        mapping(uint => string) resources;
+        mapping(address => uint) voters;
+    }
     mapping(string => Resource) private resources;
-    mapping(string => string[]) private liderbords;
+    mapping(string => Liderbord) private liderbords;
 
     function getLiderbord(string memory _liderbordName) public view returns (string[] memory, uint[] memory) {
         console.log("Getting liderbord '%s'", _liderbordName);
@@ -19,20 +27,46 @@ contract Liderbord {
         string[] memory links = new string[](n);
         uint[] memory scores = new uint[](n);
         for (uint i = 0; i < n; i++) {
-            Resource storage resource = resources[liderbords[_liderbordName][i]];
-            links[i] = liderbords[_liderbordName][i];
-            scores[i] = resource.scores[_liderbordName];
+            Resource storage resource = resources[liderbords[_liderbordName].resources[i]];
+            links[i] = liderbords[_liderbordName].resources[i];
+            scores[i] = resource.scores[_liderbordName].value;
         }
         return (links, scores);
     }
 
     function addResource(string memory _link, string[] memory _liderbordNames) public {
         console.log("Adding resource '%s'", _link);
+        // require that the resource does not exist
         Resource storage resource = resources[_link];
         for (uint i = 0; i < _liderbordNames.length; i++) {
             string memory liderbordName = _liderbordNames[i];
             resource.liderbords.push(liderbordName);
-            liderbords[liderbordName].push(_link);
+            Liderbord storage liderbord = liderbords[liderbordName];
+            resource.scores[liderbordName] = Score({value: 0, index: liderbord.length});
+            liderbord.resources[liderbord.length] = _link;
+            liderbord.length++;
         }
+    }
+
+    function deleteResource(string memory _link) public {
+        console.log("Deleting resource '%s'", _link);
+        Resource storage resource = resources[_link];
+        for (uint i = 0; i < resource.liderbords.length; i++) {
+            Liderbord storage liderbord = liderbords[resource.liderbords[i]];
+            liderbord.resources[resource.scores[resource.liderbords[i]].index] = liderbord.resources[liderbord.length - 1];
+            liderbord.length--;
+        }
+        delete resources[_link];
+    }
+
+    function vote(string memory _link, string memory _liderbordName, uint _side) public {
+        Resource storage resource = resources[_link];
+        Liderbord storage liderbord = liderbords[_liderbordName];
+        require(resource.owner != msg.sender, "Can't vote on your own resource");
+        require(liderbord.voters[msg.sender] == 0, "Can't voted in this resource");
+        require(_side != 0, "Need to vote on a side");
+        console.log("Voting for '%s' in '%s'", _link, _liderbordName);
+        liderbord.voters[msg.sender] = _side;
+        resource.scores[_liderbordName].value += _side;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -3,27 +3,70 @@ import { ethers } from "hardhat";
 
 describe("Liderbord", function () {
   it("Should add a resource", async function () {
-    const Liderbord = await ethers.getContractFactory("Liderbord");
-    const liderbord = await Liderbord.deploy();
-    await liderbord.deployed();
+    const Liderbords = await ethers.getContractFactory("Liderbords");
+    const liderbords = await Liderbords.deploy();
+    await liderbords.deployed();
 
-    const setResourceTx = await liderbord.addResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ", ["Machine Learning", "AI"]);
+    const setResourceTx = await liderbords.addResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ", ["Machine Learning", "AI"]);
 
     await setResourceTx.wait();
   });
 
   it("Should get the resources from a liderbord", async function () {
-    const Liderbord = await ethers.getContractFactory("Liderbord");
-    const liderbord = await Liderbord.deploy();
-    await liderbord.deployed();
+    const Liderbords = await ethers.getContractFactory("Liderbords");
+    const liderbords = await Liderbords.deploy();
+    await liderbords.deployed();
 
-    const setResourceTx = await liderbord.addResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ", ["Machine Learning", "AI"]);
+    const setResourceTx = await liderbords.addResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ", ["Machine Learning", "AI"]);
     await setResourceTx.wait();
 
-    const [links, scores] = await liderbord.getLiderbord("AI");
+    const [links, scores] = await liderbords.getLiderbord("AI");
     
     expect(links).to.have.lengthOf(1);
     expect(links[0]).to.be.equal("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
     expect(scores[0]).to.be.equal(0);
+  });
+
+  it("Should delete a resource", async function () {
+    const Liderbords = await ethers.getContractFactory("Liderbords");
+    const liderbords = await Liderbords.deploy();
+    await liderbords.deployed();
+
+    const setResourceTx = await liderbords.addResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ", ["Machine Learning", "AI"]);
+    await setResourceTx.wait();
+
+    const [linksWithOneValue, scoresWithOneValue] = await liderbords.getLiderbord("AI");
+    
+    expect(linksWithOneValue).to.have.lengthOf(1);
+    expect(scoresWithOneValue).to.have.lengthOf(1);
+    expect(linksWithOneValue[0]).to.be.equal("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    expect(scoresWithOneValue[0]).to.be.equal(0);
+
+    const deleteResourceTx = await liderbords.deleteResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    await deleteResourceTx.wait();
+
+    const [linksWithZeroValues, scoresWithZeroValues] = await liderbords.getLiderbord("AI");
+    
+    expect(linksWithZeroValues).to.have.lengthOf(0);
+    expect(scoresWithZeroValues).to.have.lengthOf(0);
+  });
+
+  it("Vote for a resources", async function () {
+    const Liderbords = await ethers.getContractFactory("Liderbords");
+    const liderbords = await Liderbords.deploy();
+    await liderbords.deployed();
+
+    const setResourceTx = await liderbords.addResource("https://www.youtube.com/watch?v=dQw4w9WgXcQ", ["Machine Learning", "AI"]);
+    await setResourceTx.wait();
+
+
+    const voteTx = await liderbords.vote("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "AI", 1);
+    await setResourceTx.wait();
+
+    const [links, scores] = await liderbords.getLiderbord("AI");
+    
+    expect(links).to.have.lengthOf(1);
+    expect(links[0]).to.be.equal("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    expect(scores[0]).to.be.equal(1);
   });
 });
